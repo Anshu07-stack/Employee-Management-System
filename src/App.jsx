e8b549c6847d1177637c0a8cc4fedfe5a89319bc
Anshu Kumar <venomanshu879177@gmail.com>
2026-02-17 14:22:59 +0530

    feat(auth): consume AuthContext and render dashboards based on role

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
+import { setLocalStorage } from './utils/LocalStorage'
+import { AuthContext } from './context/AuthProvider'
 
 const App = () => {
+
+  const [user,setUser] = useState(null)
+  const Authdata = useContext(AuthContext)
+  console.log(Authdata)
+
+  const handleLogin = (email,password)=>{
+      if(email === 'admin@me.com' && password === '123'){
+        setUser('admin')
+      }else if(email === 'user@me.com' && password === '123'){
+        setUser('employee')
+      }
+      else{
+        alert('invalid credentials')
+      }
+  }
+
+  
+
   return (
     <>
-    {/* <Login/> */}
-    {/* <EmployeeDashboard/> */}
-    <AdminDashboard/>
+      {!user && <Login handleLogin={handleLogin}/>}
+
+       {user === 'admin' && <AdminDashboard/>}
+
+    {user === 'employee' && <EmployeeDashboard/>}
     </>
   )
 }
