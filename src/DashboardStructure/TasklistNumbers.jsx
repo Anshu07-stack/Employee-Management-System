6ec313af840cd4666502af213b350a82d53a0f55
Anshu Kumar <venomanshu879177@gmail.com>
2026-02-18 13:48:09 +0530

    feat: display employee taskNumbers summary on dashboard

--- src/DashboardStructure/TasklistNumbers.jsx
+++ src/DashboardStructure/TasklistNumbers.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 
-const TasklistNumbers = () => {
+const TasklistNumbers = ({data}) => {
+     // console.log(data)
   return (
     <div className='flex mt-10 justify-between gap-5 w-full '>
-       <div className='py-6 px-9 w-[45%] rounded-xl bg-red-400'>
-            <h2 className='text-2xl font-semibold'>0</h2>
-            <h3 className='text-xl font-medium'>New Task</h3>
-       </div>
        <div className='py-6 px-9 w-[45%] rounded-xl bg-blue-400'>
-            <h2 className='text-2xl font-semibold'>0</h2>
+            <h2 className='text-2xl font-semibold'> {data.taskNumbers.newTask} </h2>
             <h3 className='text-xl font-medium'>New Task</h3>
        </div>
        <div className='py-6 px-9 w-[45%] rounded-xl bg-green-400'>
-            <h2 className='text-2xl font-semibold'>0</h2>
+            <h2 className='text-2xl font-semibold'> {data.taskNumbers.completed} </h2>
             <h3 className='text-xl font-medium'>New Task</h3>
        </div>
        <div className='py-6 px-9 w-[45%] rounded-xl bg-yellow-400'>
-            <h2 className='text-2xl font-semibold'>0</h2>
+            <h2 className='text-2xl font-semibold'> {data.taskNumbers.active} </h2>
+            <h3 className='text-xl font-medium'>New Task</h3>
+       </div>
+        <div className='py-6 px-9 w-[45%] rounded-xl bg-red-400'>
+            <h2 className='text-2xl font-semibold'> {data.taskNumbers.failed} </h2>
             <h3 className='text-xl font-medium'>New Task</h3>
        </div>
     </div> 
