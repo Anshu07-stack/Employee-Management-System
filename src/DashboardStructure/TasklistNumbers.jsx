0134888f08b492f97878cbef1fc30678ca7a0141
Anshu Kumar <venomanshu879177@gmail.com>
2026-02-19 22:30:52 +0530

    feat(tasklist): receive task data via props and render dynamic content

--- src/DashboardStructure/TasklistNumbers.jsx
+++ src/DashboardStructure/TasklistNumbers.jsx
@@ -5,8 +5,8 @@ const TasklistNumbers = ({data}) => {
   return (
     <div className='flex mt-10 justify-between gap-5 w-full '>
        <div className='py-6 px-9 w-[45%] rounded-xl bg-blue-400'>
-            <h2 className='text-2xl font-semibold'> {data.taskNumbers.newTask} </h2>
-            <h3 className='text-xl font-medium'>New Task</h3>
+            <h2 className='text-2xl font-semibold text-white'> {data.taskNumbers.newTask} </h2>
+            <h3 className='text-xl font-medium text-white'>New Task</h3>
        </div>
        <div className='py-6 px-9 w-[45%] rounded-xl bg-green-400'>
             <h2 className='text-2xl font-semibold'> {data.taskNumbers.completed} </h2>
