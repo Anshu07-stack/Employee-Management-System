4c58a480fa2476c0bf91e575076ce87ec83cb2c1
Anshu Kumar <venomanshu879177@gmail.com>
2026-02-19 22:25:17 +0530

    feat(AcceptTask): receive task data via props and render dynamic content

--- src/components/TaskList/CompleteTask.jsx
+++ src/components/TaskList/CompleteTask.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 
-const CompleteTask = () => {
+const CompleteTask = ({data}) => {
   return (
     <div className="shrink-0 h-full w-75 bg-blue-400 rounded-xl p-5">
       <div className="flex justify-between items-center">
-        <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
-        <h4 className="text-sm">16 feb 2026</h4>
+        <h3 className="bg-red-600 text-sm px-3 py-1 rounded text-white">{data.category}</h3>
+        <h4 className="text-sm text-white">{data.taskDate}</h4>
       </div>
-      <h2 className="mt-5 text-2xl font-semibold">Make a YT video</h2>
-      <p className="text-sm mt-2 ">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit expedita
-        sunt, dicta facere totam odio?
+      <h2 className="mt-5 text-2xl font-semibold text-white">{data.taskTitle}</h2>
+      <p className="text-sm mt-2 text-white ">
+        {data.taskDescription}
       </p>
       <div className="mt-2">
-            <button className="w-full">Completed</button>
+            <button className="w-full text-white">Completed</button>
       </div>
     </div>
   );
