d84bc934a8edb1d379e95b0a789451eb400ec24f
Anshu Kumar <venomanshu879177@gmail.com>
2026-02-19 22:23:00 +0530

    style(AllTask): update table styling, add borders and colored task counters

--- src/DashboardStructure/AllTask.jsx
+++ src/DashboardStructure/AllTask.jsx
@@ -1,31 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../context/AuthProvider'
 
 const AllTask = () => {
+  const authData = useContext(AuthContext)
+  console.log(authData.employees)
+
   return (
-    <div className='bg-[#252525] rounded p-5 mt-5 h-40  overflow-auto' >
-      <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded' >
-        <h2>Anshu</h2>
-        <h3>Make UI design</h3>
-        <h5>Status</h5>
+<div className='bg-[#252525] rounded p-5 mt-5 h-60' >
+   <div 
+   className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded text-white' >
+                     <h2 className='text-lg font-semibold   w-1/5 '>Employee Name</h2>
+                     <h3 className='text-lg font-semibold  w-1/5'>New Tasks</h3>
+                     <h5 className='text-lg font-semibold  w-1/5 '> Active Tasks</h5>
+                     <h5 className='text-lg font-semibold  w-1/5 '>Completed Tasks</h5>
+                     <h5 className='text-lg font-semibold  w-1/5 '>Failed Tasks</h5>
       </div>
-      
-       <div className='bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded' >
-        <h2>Anshu</h2>
-        <h3>Make UI design</h3>
-        <h5>Status</h5>
-      </div>
-
-       <div className='bg-green-400 mb-2 py-2 px-4 flex justify-between rounded' >
-        <h2>Anshu</h2>
-        <h3>Make UI design</h3>
-        <h5>Status</h5>
-      </div >
-
-       <div className='bg-yellow-400 mb-2 py-2 px-4 flex justify-between rounded' >
-        <h2>Anshu</h2>
-        <h3>Make UI design</h3>
-        <h5>Status</h5>
-      </div>
+        <div className='h-[80%] overflow-auto'>
+          {authData.employees.map((elem , id)=>{
+          return  <div key={id} className='border border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded' >
+                     <h2 className='text-lg w-1/5 text-white font-medium'>{elem.firstname}</h2>
+                     <h3 className='text-lg w-1/5 text-blue-600 font-semibold'>{elem.taskNumbers.active}</h3>
+                     <h5 className='text-lg w-1/5 text-yellow-400 font-semibold'>{elem.taskNumbers.newTask}</h5>
+                     <h5 className='text-lg w-1/5 text-green-400 font-semibold'>{elem.taskNumbers.completed}</h5>
+                     <h5 className='text-lg w-1/5 text-red-600 font-semibold'>{elem.taskNumbers.failed}</h5>
+      </div>  
+          
+        })}
+        </div>
     </div>
   )
 }
